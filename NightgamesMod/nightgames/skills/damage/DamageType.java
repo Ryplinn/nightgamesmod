a54752c5eea1ca9200fbd7ab07f6fc13b8652ef4
Ryplinn <ryplinn@gmail.com>
2019-07-09 23:28:52 -0700

    Rescales offensive and defensive power to simplify calculations.
    Offensive power points were increasing damage by 3% each, and defensive
    power points were reducing damage by 1.5% each. However, most of the
    power point calculations were dividing offensive points by 3 and
    defensive points by 2. I baked those divisions into the point -> percent
    conversions instead of the point calculations, so now offensive power
    points increase damage by 1% and defensive points reduce damage by 0.75%
    instead.

--- NightgamesMod/nightgames/skills/damage/DamageType.java
+++ NightgamesMod/nightgames/skills/damage/DamageType.java
@@ -30,72 +30,75 @@ public enum DamageType {
     ;
 
     /**
-     * Every point of defensive power reduces incoming damage by 1.5%.
+     * Every point of defensive power reduces incoming damage by .75%
      *
      * @param target The character receiving the damage
      * @return The characters defensive power against the damage type
      */
     public double getDefensivePower(Character target){
         switch (this) {
+            // TODO: consider adding vulnerabilities
+            // arcane disrupts gadgets? dark vulnerable to divine? asphyxiation prevents ki breath control?
             case arcane:
-                // (2 * arcane + dark + divinity + ki) / 2
+                // (2 * arcane + dark + divinity + ki)
                 // each point of arcane reduces incoming damage by 1.5%
                 // each point of the other attributes reduces incoming damage by .75%
-                return target.get(Attribute.spellcasting) + target.get(Attribute.darkness) / 2.0 + target.get(Attribute.divinity) / 2.0 + target
-                                .get(Attribute.ki) / 2.0;
+                return 2 * target.get(Attribute.spellcasting) + target.get(Attribute.darkness) + target.get(Attribute.divinity) +
+                                target.get(Attribute.ki);
             case biological:
                 // (animism + bio + medicine + science + cunning + seduction) / 2
                 // each point of these attributes reduces incoming damage by .75%
-                return target.get(Attribute.animism) / 2.0 + target.get(Attribute.bio) / 2.0 + target.get(Attribute.medicine) / 2.0 + target
-                                .get(Attribute.science) / 2.0 + target.get(Attribute.cunning) / 2.0 + target.get(Attribute.seduction) / 2.0;
+                return target.get(Attribute.animism) + target.get(Attribute.bio) + target.get(Attribute.medicine) + target
+                                .get(Attribute.science) + target.get(Attribute.cunning) + target.get(Attribute.seduction);
             case pleasure:
                 // each point of seduction reduces incoming damage by 1.5%
-                return target.get(Attribute.seduction);
+                return 2 * target.get(Attribute.seduction);
             case temptation:
                 // (2 * seduction + 2 * submissive + cunning) / 2
                 // each point of seduction or submission reduces incoming damage by 1.5%
                 // cunning reduces incoming damage by .75%
-                return (target.get(Attribute.seduction) * 2 + target.get(Attribute.submission) * 2 + target.get(Attribute.cunning)) / 2.0;
+                return target.get(Attribute.seduction) * 2 + target.get(Attribute.submission) * 2 + target.get(Attribute.cunning);
             case technique:
                 // each point of cunning reduces incoming damage by 1.5%
-                return target.get(Attribute.cunning);
+                return 2 * target.get(Attribute.cunning);
             case physical:
-                // (2 * power + cunning) / 2
+                // (2 * power + cunning)
                 // each point of power reduces incoming damage by 1.5%
                 // each point of cunning reduces incoming damage by 0.75%
-                return (target.get(Attribute.power) * 2 + target.get(Attribute.cunning)) / 2.0;
+                return (target.get(Attribute.power) * 2 + target.get(Attribute.cunning));
             case gadgets:
                 // each point of cunning reduces incoming damage by 1.5%
-                return target.get(Attribute.cunning);
+                return 2 * target.get(Attribute.cunning);
             case drain:
-                // (2 * dark + arcane) / 2
+                // (2 * dark + arcane)
                 // each point of dark reduces incoming damage by 1.5%
                 // each point of arcane reduces incoming damage by 0.75%
-                return (target.get(Attribute.darkness) * 2 + target.get(Attribute.spellcasting)) / 2.0;
+                return target.get(Attribute.darkness) * 2 + target.get(Attribute.spellcasting);
             case stance:
-                // (2 * cunning + power) / 2
+                // TODO: stance damage should not be used for both smothering (e.g., from facesitting) and gradual exhaustion (e.g., from carrying someone while fucking them).
+                // (2 * cunning + power)
                 // each point of cunning reduces incoming damage by 1.5%
                 // each point of power reduces incoming damage by 0.75%
-                return (target.get(Attribute.cunning) * 2 + target.get(Attribute.power)) / 2.0;
+                return target.get(Attribute.cunning) * 2 + target.get(Attribute.power);
             case weaken:
-                // (2 * dark + divinity) / 2
+                // (2 * dark + divinity)
                 // each point of dark reduces incoming damage by 1.5%
                 // each point of power reduces incoming damage by .75%
-                return (target.get(Attribute.darkness) * 2 + target.get(Attribute.divinity)) / 2.0;
+                return target.get(Attribute.darkness) * 2 + target.get(Attribute.divinity);
             case willpower:
-                // (dark + fetish + 2 * divinity + level) / 2
+                // (dark + fetish + 2 * divinity + level)
                 // each level reduces incoming damage by .75%
                 // each point of divinity reduces incoming damage by 1.5%
                 // each point of fetish or dark reduces incoming damage by .75%
-                return (target.get(Attribute.darkness) + target.get(Attribute.fetishism) + target.get(Attribute.divinity) * 2 + target
-                                .getLevel()) / 2.0;
+                return target.get(Attribute.darkness) + target.get(Attribute.fetishism) + target.get(Attribute.divinity) * 2 + target
+                                .getLevel();
             default:
                 return 0;
         }
     }
 
     /**
-     * Every point of offensive power increases outgoing damage by 3%.
+     * Every point of offensive power increases outgoing damage by 1%.
      *
      * @param source The character causing the damage
      * @return The character's offensive power of the damage type
@@ -103,86 +106,92 @@ public enum DamageType {
     public double getOffensivePower(Character source){
         switch (this) {
             case biological:
-                // (animism + bio + medicine + science) / 2
-                // each point of these attributes increases outgoing damage by 1.5%
-                return (source.get(Attribute.animism) + source.get(Attribute.bio) + source.get(Attribute.medicine) + source
-                                .get(Attribute.science)) / 2.0;
+                // 2 * animism + 2 * bio + medicine + science
+                // each point of animism or bio increases outgoing damage by 2%
+                // each point of medicine or science increases outgoing damage by 1%
+                return 2 * source.get(Attribute.animism) + 2 * source.get(Attribute.bio) + source.get(Attribute.medicine) + source
+                                .get(Attribute.science);
             case gadgets:
-                // (2 * science + cunning) / 3 + 20(has toymaster)
+                // (2 * science + cunning) + toymaster ? 60
                 // each point of science increases outgoing damage by 2%
                 // each point of cunning increases outgoing damage by 1%
                 // toymaster trait increases outgoing damage by 60%
-                double power = (source.get(Attribute.science) * 2 + source.get(Attribute.cunning)) / 3.0;
-                if (source.has(Trait.toymaster)) {
-                    power += 20;
-                }
-                return power;
+                return (source.get(Attribute.science) * 2 + source.get(Attribute.cunning) + (source.has(Trait.toymaster) ? 60 : 0));
             case pleasure:
                 // each point of seduction increases outgoing damage by 3%
-                return source.get(Attribute.seduction);
+                return 3 * source.get(Attribute.seduction);
             case arcane:
                 // each point of arcane increases outgoing damage by 3%
-                return source.get(Attribute.spellcasting);
+                return 3 * source.get(Attribute.spellcasting);
             case temptation:
-                // (2 * seduction + cunning) / 3
+                // 2 * seduction + cunning
                 // each point of seduction increases outgoing damage by 2%
                 // each point of cunning increases outgoing damage by 1%
-                return (source.get(Attribute.seduction) * 2 + source.get(Attribute.cunning)) / 3.0;
+                return source.get(Attribute.seduction) * 2 + source.get(Attribute.cunning);
             case technique:
                 // each point of cunning increases outgoing damage by 3%
-                return source.get(Attribute.cunning);
+                return 3 * source.get(Attribute.cunning);
             case physical:
-                // (2 * power + cunning + 2 * ki) / 3
+                // 2 * power + cunning + 2 * ki
                 // each point of power or ki increases outgoing damage by 2%
                 // each point of cunning increases outgoing damage by 1%
                 // power represents raw strength
                 // cunning represents weakpoint targeting
                 // ki represents showing them your moves
-                return (source.get(Attribute.power) * 2 + source.get(Attribute.cunning) + source.get(Attribute.ki) * 2) / 3.0;
+                return source.get(Attribute.power) * 2 + source.get(Attribute.cunning) + source.get(Attribute.ki) * 2;
             case drain:
                 // with gluttony:
-                // (4 * dark + 2 * arcane) / 3
+                // (4 * dark + 2 * arcane)
                 // each point of dark increases outgoing damage by 4%
                 // each point of arcane increases outgoing damage by 2%
                 // without gluttony:
-                // (2 * dark + arcane) / 2
+                // (2 * dark + arcane)
                 // each point of dark increases outgoing damage by 3%
                 // each point of arcane increases outgoing damage by 1.5%
                 // ergo:
                 // gluttony increases drain power by 33%
-                return (source.get(Attribute.darkness) * 2 + source.get(Attribute.spellcasting)) / (source.has(Trait.gluttony) ? 1.5 : 2.0);
+                // TODO: Move gluttony trait effect to drain damage method instead of type.
+                if (source.has(Trait.gluttony)) {
+                    return 4 * source.get(Attribute.darkness) + 2 * source.get(Attribute.spellcasting);
+                } else {
+                    return (source.get(Attribute.darkness) * 3 + 1.5 * source.get(Attribute.spellcasting));
+                }
             case stance:
-                // (2 * cunning + power) / 3
+                // (2 * cunning + power)
                 // each point of cunning increases outgoing damage by 2%
                 // each point of power increases outgoing damage by 1%
-                return (source.get(Attribute.cunning) * 2 + source.get(Attribute.power)) / 3.0;
+                return source.get(Attribute.cunning) * 2 + source.get(Attribute.power);
             case weaken:
-                // (2 * dark + divinity + ki) / 3
+                // (2 * dark + divinity + ki)
                 // each point of dark increases outgoing damage by 2%
                 // each point of divinity or ki increases outgoing damage by 1%
-                return (source.get(Attribute.darkness) * 2 + source.get(Attribute.divinity) + source.get(Attribute.ki)) / 3.0;
+                return source.get(Attribute.darkness) * 2 + source.get(Attribute.divinity) + source.get(Attribute.ki);
             case willpower:
-                // (dark + fetish + divinity * 2 + level) / 3
+                // (dark + fetish + 2 * divinity + level)
                 // each point of dark or fetish increases outgoing damage by 1%
                 // each point of divinity increases outgoing damage by 2%
                 // each level increases outgoing damage by 1%
-                return (source.get(Attribute.darkness) + source.get(Attribute.fetishism) + source.get(Attribute.divinity) * 2 + source
-                                .getLevel()) / 3.0;
+                return source.get(Attribute.darkness) + source.get(Attribute.fetishism) + source.get(Attribute.divinity) * 2 + source
+                                .getLevel();
+                return 3 * source.get(Attribute.divinity) + source.get(Attribute.spellcasting);
             default:
                 return 0;
         }
     }
 
     public double modifyDamage(Character source, Character target, double baseDamage) {
-        // so for each damage type, one level from the attacker should result in about 3% increased damage, while a point in defense should reduce damage by around 1.5% per level.
-        // this differential should be max capped to (2 * (100 + attacker's level * 1.5))%
-        // this differential should be min capped to (.5 * (100 + attacker's level * 1.5))%
+        // damage should be max capped to (2 * (100 + attacker's level * 1.5))%
+        // damage should be min capped to (.5 * (100 + attacker's level * 1.5))%
         double maxDamage = baseDamage * 2 * (1 + .015 * source.getLevel());
         double minDamage = baseDamage * .5 * (1 + .015 * source.getLevel());
-        double multiplier = (1 + .03 * getOffensivePower(source) - .015 * getDefensivePower(target));
+        double offensivePower = getOffensivePower(source);
+        double defensivePower = getDefensivePower(target);
+        double multiplier = (1 + .01 * offensivePower - .0075 * defensivePower);
         if (DebugFlags.isDebugOn(DebugFlags.DEBUG_DAMAGE)) {
-            System.out.println(baseDamage + " from " + source.getTrueName() + " has multiplier " + multiplier + " against " + target.getTrueName() + " ["+ getOffensivePower(
-                            source) +", " + getDefensivePower(target) + "].");
+            System.out.println(
+                            baseDamage + " from " + source.getTrueName() + " has multiplier " + multiplier + " against "
+                                            + target.getTrueName() + " [" + offensivePower + ", " + defensivePower
+                                            + "].");
         }
         double damage = baseDamage * multiplier;
         return Math.min(Math.max(minDamage, damage), maxDamage);
